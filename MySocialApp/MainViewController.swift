ab966e19184ffbeaaa2d208627040f113d634ff8
HugoVP <dev@hugo.plus>
2017-07-29 12:50:59 -0500

    Init commit & project structure
    ...

---
+++ MySocialApp/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  MySocialApp
+//
+//  Created by Hugo on 29/07/17.
+//  Copyright © 2017 Hugo. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
