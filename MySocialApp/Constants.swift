be26503cdbadc8c0e8c1b127cc74646d0a1dcd48
HugoVP <dev@hugo.plus>
2017-07-31 14:07:56 -0500

    Sing In UI custom classes complete

---
+++ MySocialApp/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  MySocialApp
+//
+//  Created by Hugo on 31/07/17.
+//  Copyright © 2017 Hugo. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
