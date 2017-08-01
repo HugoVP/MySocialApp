38871fefaa4364aada8bebf2c3992871f5ecc896
HugoVP <dev@hugo.plus>
2017-08-01 01:39:00 -0500

    Facebook Auth complete

--- MySocialApp/MainViewController.swift
+++ MySocialApp/MainViewController.swift
@@ -7,9 +7,38 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+    @IBAction func fbButtonPressed(_ sender: UIButton) {
+        let fbLoginManager = FBSDKLoginManager()
+
+        fbLoginManager.logIn(withReadPermissions: ["email"], from: self) { (result, err) in
+            if err != nil {
+                print("Unable to authenticate with Facebook")
+            } else if (result?.isCancelled)! {
+                print("User cancelled Facebook authentication")
+            } else {
+                print("Successfully authenticated with Facebook")
+                let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+
+    func firebaseAuth(_ credencial: AuthCredential) {
+        Auth.auth().signIn(with: credencial) { (user, err) in
+            if err != nil {
+                print("Unable to authenticate with Firebase: \(String(describing: err))")
+            } else {
+                print("Successfully authenticated with Firebase")
+            }
+        }
     }
 }
